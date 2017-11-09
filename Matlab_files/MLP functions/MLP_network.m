061d363a39e8543d8ec81274239057f27dde4427
Thomas Hayden <thomashayden@167-89.priv23.nus.edu.sg>
2017-11-09 14:21:25 +0800

    'Finished' Lit review. Added Learning rate data.

--- Matlab_files/MLP functions/MLP_network.m
+++ Matlab_files/MLP functions/MLP_network.m
@@ -6,10 +6,15 @@
 options.trainsize = 20000;       %value between 1 and 50000
 options.preprocessing = 'all'; %valid values are 'none' and 'all'
 options.trainFcn =  'trainscg';
-options.layers = [0 0];
-
+options.augmentation = 'none';
+options.layers = [100 50];
+options.learningRate = 0.01;
 %First aquire all data. This should not be changed.
 dataAquisition();
+
+option.trainFcn = '';
+
+trainFcn = {'traincgb','traincgf','traincgp','traingd','traingda','traingdm','traingdx','trainoss','trainrp','trainscg','trainb','trainc','trainr','trains','trainbu','trainru'};
 
 %[cifarData2, mu, invMat, whMat] = whiten(cifarData);
 
@@ -19,20 +24,18 @@ dataAquisition();
 
 % [net, sucessRateTraining] = networkTraining(50,cifarDataMirrored,cifarLabelsMirrored);
 % sucessRateTesting = networkTesting(net, cifarData(trainSize+1:testEnd,:), cifarLabels(trainSize+1:testEnd,:));
-sucessRateTesting = zeros(9,9,5);
-trainTime = zeros(9,9,5);
+sucessRateTesting = zeros(6,5);
+trainTime = zeros(6,5);
 
-for i = 1 : 9
-    options.layers = [0 options.layers(2)+10];
-    for j = 1 : 9
-        options.layers = [options.layers(1)+10 options.layers(2)];
-        for k = 1 : 5
-            [Data_cpy, Labels_cpy, test_cpy] = dataPreProcAndAug(cifarData,cifarLabels,options);
-            tic
-            [net, sucessRateTraining] = networkTraining(Data_cpy,Labels_cpy,options);
-            sucessRateTesting(i,j,k) = networkTesting(net, test_cpy, cifarLabels(50001:60000,:));
-            trainTime(i,j,k) = toc;
-        end
+for i = 1 : 12
+    options.trainFcn = trainFcn{i};
+    for j = 1 : 5
+        
+        [Data_cpy, Labels_cpy, test_cpy] = dataPreProcAndAug(cifarData,cifarLabels,options);
+        tic
+        [net, sucessRateTraining] = networkTraining(Data_cpy,Labels_cpy,options);
+        sucessRateTesting(i,j) = networkTesting(net, test_cpy, cifarLabels(50001:60000,:));
+        trainTime(i,j) = toc;
     end
 end
 
