b4d7d4c7818064786a2d83412e1f567537a54296
Mario Gini <mgini@student.ethz.ch>
2017-11-10 11:25:25 +0800

    CNN report

--- Matlab_files/CNN_functions/CNN_script.m
+++ Matlab_files/CNN_functions/CNN_script.m
@@ -59,20 +59,18 @@ layers = [
     convolution2dLayer(3,16,'Padding',1)
     batchNormalizationLayer
     reluLayer
-
     maxPooling2dLayer(2,'Stride',2)
 
     convolution2dLayer(3,32,'Padding',1)
     batchNormalizationLayer
     reluLayer
-
     maxPooling2dLayer(2,'Stride',2)
 
     convolution2dLayer(3,64,'Padding',1)
     batchNormalizationLayer
     reluLayer
-
     fullyConnectedLayer(10)
+    
     softmaxLayer
     classificationLayer];
  
